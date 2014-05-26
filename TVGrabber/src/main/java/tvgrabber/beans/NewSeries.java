1d3616c6456203d5424a562dade04035dc814f8f
LeBon <stammlersimon@gmx.de>
2014-05-26 21:23:15 +0200

    implemented newSeries filter and added test, refined twitter

--- TVGrabber/src/main/java/tvgrabber/beans/NewSeries.java
+++ TVGrabber/src/main/java/tvgrabber/beans/NewSeries.java
@@ -1,7 +1,17 @@
 package tvgrabber.beans;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Headers;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
+import tvgrabber.TVGrabberMain;
+import tvgrabber.entities.Series;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 /**
  * Created by LeBon on 25.05.14.
@@ -9,22 +19,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class NewSeries {
 
+    private static final Logger logger = Logger.getLogger(NewSeries.class);
 
     private int count = 1;
+    private Connection con;
 
     /**
      * checks if serie is new
+     *
      * @param exchange
      * @return
      */
-    public boolean filterExistingSeries(Exchange exchange) {
-        //TODO implement
-        if (count == 1) {
-            count++;
+    public boolean filterExistingSeries(@Headers Map<String, Object> headers, Exchange exchange) {
+
+        Series serie2check = exchange.getIn().getBody(Series.class);
+
+        try {
+            if (con == null){
+                con = TVGrabberMain.getConnection();
+            }
+            PreparedStatement countSeriesWithSameName =
+                    con.prepareStatement("SELECT count(*) from TVGRABBER.TVProgram where title LIKE ?");
+            countSeriesWithSameName.setString(1, "%"+serie2check.getTitle()+"%");
+            ResultSet rsSerie = countSeriesWithSameName.executeQuery();
+
+            rsSerie.next();
+            count = rsSerie.getInt(1);
+            logger.debug("How many similar names: " + count + " " + serie2check.getTitle());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (count == 0) {
             return true;
-        }           else{
+        } else {
             return false;
         }
     }
 
+    public void setCon(Connection con) {
+        this.con = con;
+    }
 }
--- TVGrabber/src/test/resources/testinsert.sql
+++ TVGrabber/src/test/resources/testinsert.sql
@@ -2,3 +2,14 @@ insert into TVGRABBER.TVUser
 (email, subscribed, searchTerm) values ('bla', FALSE , '');
 insert into TVGRABBER.TVUser
 (email, subscribed, searchTerm) values ('usermailbiz', TRUE , 'subscribed');
+
+
+insert into TVGRABBER.TVProgram
+(title, description, startTime, endTime, channel)
+values ('test1', 'just a sample', '1990-03-02 08:30:00.010000','1990-03-02 08:30:00.020000', 'blachannel');
+insert into TVGRABBER.TVProgram
+(title, description, startTime, endTime, channel)
+values ('test2', 'just a sample', '1990-03-02 08:30:00.010000','1990-03-02 08:30:00.020000', 'blachannel');
+insert into TVGRABBER.TVProgram
+(title, description, startTime, endTime, channel)
+values ('test2blabla', 'just a sample', '1990-03-02 08:30:00.010000','1990-03-02 08:30:00.020000', 'blachannel');
