69b1912f7aa28ab687dac57791b02d1ae8b036f6
Felder Clemens <e1026501@student.tuwien.ac.at>
2014-05-28 10:14:46 +0200

    Imporved tests

--- TVGrabber/src/test/java/tvgrabber/beans/AddressmanagerTest.java
+++ TVGrabber/src/test/java/tvgrabber/beans/AddressmanagerTest.java
@@ -32,7 +32,7 @@ import java.util.Map;
         classes = {TVGrabberConfig.class, StandAloneTestH2.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @ActiveProfiles("testing")
-public class TVGrabberSubscribeTest extends CamelTestSupport {
+public class AddressmanagerTest extends CamelTestSupport {
 
     private String myBody;
     private Map<String, Object> header;
@@ -66,7 +66,6 @@ public class TVGrabberSubscribeTest extends CamelTestSupport {
 
     }
 
-    /* Does not work; I guess because the db is "hard" coded..
     @Test
     public void testUnsubscribe(){
         header.put("from","usermailbiz");
@@ -79,7 +78,6 @@ public class TVGrabberSubscribeTest extends CamelTestSupport {
         assertNotEquals(myBody, exchange.getOut().getBody(TVGrabberUser.class).getSearchTerm());
         assertFalse(exchange.getOut().isFault());
     }
-     */
 
     @Test
     public void unsubscribeExpectedException(){
